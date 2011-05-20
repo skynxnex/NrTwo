f6faf807cb01bdbdb6963e97a88e7e33165875f6
Erik Perttu <erik.perttu@gmail.com>
2011-05-20 16:09:40 +0200

    More Structrue plus basic js and php funcitons. Some working some not working as intended.

---
+++ includes/php/include.php
@@ -0,0 +1,19 @@
+<?php
+
+if (isset($_GET['action']) && $_GET['action'] == 'dummyJSON') {
+
+    //$query = "SQL QUERY";
+    //$something->calls($query);
+
+    header('Content-Type: application/json; charset=utf-8');
+    $result = '{
+        "table1": "Table1 data",
+        "table2": "Table2 data",
+        "table3": "Table3 data",
+        "table4": "Table4 data",
+        "table5": "Table5 data",
+        "table6": "Table6 data"
+    }';
+    echo $result;
+}
+?>
--- index.php
+++ index.php
@@ -1,16 +1,21 @@
+<?php
+include "includes/php/include.php"
+?>
 <!DOCTYPE html>
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
+        <link href="style/style.css" rel="stylesheet" type="text/css">
+        <title>.:..:.</title>
     </head>
     <body>
-    	<p>Detta har Anneli skrivit. CPP (COMMIT PULL PUSH)</p>
+        <p>Detta har Anneli skrivit. CPP (COMMIT PULL PUSH)</p>
+        <div id="opopin"></div>        
         <?php
-        // put your code here
-        //new comment
+        include "includes/html/inc_topnav.html"
         ?>
         <script type="text/javascript" src="js/jquery/jquery-1.6.1.min.js"></script>
         <script type="text/javascript" src="js/jquery/jquery-ui-1.8.13.custom.min.js"></script>
+        <script type="text/javascript" src="js/general.js"></script>
     </body>
 </html>
