93282a50edafc6a707562d1228123ca90b2a7e8e
Erik Perttu <erik.perttu@gmail.com>
2011-05-20 14:05:05 +0200

    Structure +local js files and dynamicHandler.php

--- index.php
+++ index.php
@@ -10,5 +10,7 @@
         // put your code here
         //new comment
         ?>
+        <script type="text/javascript" src="js/jquery/jquery-1.6.1.min.js"></script>
+        <script type="text/javascript" src="js/jquery/jquery-ui-1.8.13.custom.min.js"></script>
     </body>
 </html>
