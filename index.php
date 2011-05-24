ad8e7f2b2c542b618ca8c710a74e9e7290b5f6a4
Pontus Alm <pontus@almar.se>
2011-05-24 11:07:19 +0200

    updated index.php and added api/index.php where all functions are run thru

--- index.php
+++ index.php
@@ -3,22 +3,3 @@
 require_once('config/config.php');
 $html = file_get_contents(INCLUDE_PATH.'/includes/html/index.html');
 echo $html;
-
-if(isset($_GET['action'])) {
-	$action = $_GET['action'];
-	$file = INCLUDE_PATH.'library/'.$_GET['action'].'.func.php';
-	$args = "";
-
-	if(isset($_GET['args'])) {
-		$args = $_GET['args'];
-	} else {
-		echo "Need arguments for function";
-	}
-	
-	if(file_exists($file)) {
-		include($file);
-		echo json_encode(call_user_func($action, $args));
-	} else {
-		echo "no such file or function ".$action;
-	}
-}
--- library/test.func.php
+++ library/test.func.php
@@ -2,7 +2,7 @@
 
 // Testing include in index.php, delete when function works
 
-function test($args = null) {
+function _test($args = null) {
 	echo "testfunction run properly";
 	$array = array();
 	return $array;
