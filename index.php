a85dd9ad745bd186377ef48840feb820cec9fd71
Pontus Alm <pontus@almar.se>
2011-05-24 09:01:25 +0200

    index now grabs params from url and runs the appropriate function with args

--- index.php
+++ index.php
@@ -1,6 +1,24 @@
 <?php
 
-
 require_once('config/config.php');
-echo "index.php \n";
+$html = file_get_contents(INCLUDE_PATH.'/includes/html/index.html');
+echo $html;
 
+if(isset($_GET['action'])) {
+	$action = $_GET['action'];
+	$file = INCLUDE_PATH.'library/'.$_GET['action'].'.func.php';
+	$args = "";
+
+	if(isset($_GET['args'])) {
+		$args = $_GET['args'];
+	} else {
+		echo "Need arguments for function";
+	}
+	
+	if(file_exists($file)) {
+		include($file);
+		echo json_encode(call_user_func($action, $args));
+	} else {
+		echo "no such file or function ".$action;
+	}
+}
--- library/test.func.php
+++ library/test.func.php
@@ -1,3 +1,10 @@
 <?php
 
 // Testing include in index.php, delete when function works
+
+function test($args = null) {
+	echo "testfunction run properly";
+	$array = array();
+	return $array;
+
+}
