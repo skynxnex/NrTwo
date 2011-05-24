ad7b629ad4e6fdc6f5ec2da420a33d3c16594db6
Pontus Alm <pontus@almar.se>
2011-05-24 15:00:53 +0200

    added script to update db from sql file

---
+++ sql/nrtwo.sql
@@ -0,0 +1,18 @@
+-- Adminer 3.2.2 MySQL dump
+
+SET NAMES utf8;
+SET foreign_key_checks = 0;
+SET time_zone = 'SYSTEM';
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+DROP DATABASE IF EXISTS `nrtwo`;
+CREATE DATABASE `nrtwo` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_bin */;
+USE `nrtwo`;
+
+DROP TABLE IF EXISTS `test`;
+CREATE TABLE `test` (
+  `test` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+
+-- 2011-05-24 14:47:16
