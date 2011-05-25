7be86091013efe702063a4a83ed860795c7d3ce9
Erik Perttu <erik.perttu@gmail.com>
2011-05-25 14:36:14 +0200

    addExpertise.func.php

---
+++ api/includes/addExpertise.func.php
@@ -0,0 +1,34 @@
+<?php
+/*param: array(string, string, int), inserts into db, returns: status, insertid,errormsg*/
+
+require_once('../config/config.php');
+require_once(INCLUDE_PATH.'/api/includes/MysqliConnect.php');
+
+function _addExpertise($args = array()){
+	
+	
+	$connect = new MysqliConnect;
+	$db = $connect->dbConnect();
+
+	$query = "INSERT INTO persons (firstname, lastname) VALUES (?,?)";
+	$stmt = $db->prepare($query);
+	
+	$firstname = $args['firstname'];
+	$lastname = $args['lastname'];
+	$stmt->bind_param("ss", $firstname, $lastname);
+	
+	if($stmt->execute()) {
+		return array(	'status' => 'success',
+						'id' => $stmt->insert_id,
+						'error' => $stmt->error
+					);
+	}else{
+		return array(	'status' => 'fail',
+						'id' => $stmt->insert_id,
+						'error' => $stmt->error
+					);
+		}
+
+}
+
+
