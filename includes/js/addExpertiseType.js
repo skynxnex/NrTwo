7ba05e3688f216b66e13809f63ed35e846aaf5d1
maeertin <martin.barri@hotmail.com>
2011-05-26 14:00:22 +0200

    ändring

--- includes/js/addExpertiseType.js
+++ includes/js/addExpertiseType.js
@@ -1,42 +1,37 @@
-$(function() {
-	$('#createExpertiseType').click(function(){
-		$('#main_body').empty();
-		$('#main_body').load('includes/html/formCreateExpertiseType.html', newExpertiseType);
-					
-		function newExpertiseType(){
-			$('.error').hide();
-			$('input#name').select().focus();
-			
-			$('.submit').click(function(){
-				$('.error').hide();
-				var name = $('input#name').val();
-				if(name==""){
-					$("label#name_error").show();
-					$("input#name").focus();
-					return false;
-				}
-				
-				var dataString = 'name=' + name;	
-			$.ajax({
-				type: "POST",
-				url: "api/?/addExpertiseType",
-				dataType: 'json',
-                                data: dataString,
-				success: function() {
-						$('#createExpertiseType').html("<div id='message'></div>");
-						$('#message').html("<h2>Kompetenstyp inlagd i systemet. :D</h2>")
-						.append("<p></p>")
-						.hide()
-						.fadeIn(1500, function() {
-							$('#message').append("<img id='checkmark' src='/images/check.png' />");
-						});
-					}
-				});
-				return false;
-			});
+function addExpertiseType(){
+	
+	$('.error').hide();
+	$('input#name').select().focus();
+	
+	$('.submit').click(function(){
+		// validate and process form
+		// first hide any error messages
+		$('.error').hide();
+		var name = $('input#name').val();
+		if(name==""){
+			$("label#name_error").show();
+			$("input#name").focus();
+			return false;
+		}
 		
-		};
-
+		var dataString = 'name=' + name;
+		
+		$.ajax({
+			type: "POST",
+			url: "api/?/addExpertiseType",
+			dataType : 'json',
+			data: dataString,
+			success: function() {
+				$('#formHolder').html("<div id='message'></div>");
+				$('#message').html("<h2>Kompetenstyp inlagd i systemet :D</h2>")
+				.append("<p>Vi hör av oss.</p>")
+				.hide()
+				.fadeIn(1500, function() {
+					$('#message').append("<img id='checkmark' src='/images/check.png' />");
+				});
+			}
+		});
+		return false;
 	});
 
-});
+}
