01e65ef486c0f048cb45a53096fe996d60f2da9f
maeertin <martin.barri@hotmail.com>
2011-05-26 11:41:48 +0200

    gjorde om det som en funktion som anropas från general.js

--- includes/js/addConsultant.js
+++ includes/js/addConsultant.js
@@ -1,53 +1,45 @@
-$(function() {
-	$('#addConsult').click(function() {
-		$('#main_body').empty();
-		$('#main_body').load('includes/html/form_add_consultant.html', new_user);
+function new_consult(){
+		
+	$('.error').hide();
+	$("input#firstname").select().focus();
 
-		function new_user(){
-				
-			$('.error').hide();
-			$("input#firstname").select().focus();
-		
-			$(".submit").click(function() {
-				// validate and process form
-				// first hide any error messages
-				$('.error').hide();
-				var firstname = $("input#firstname").val();
-				if (firstname == "") {
-					$("label#firstname_error").show();
-					$("input#firstname").focus();
-					return false;
-				}
-				var lastname = $("input#lastname").val();
-					if (lastname == "") {
-					$("label#lastname_error").show();
-					$("input#lastname").focus();
-					return false;
-				}
-		
-				var dataString = 'firstname='+ firstname + '&lastname=' + lastname;
-				alert (dataString); return false;
-		
-				$.ajax({
-					type: "POST",
-					url: "api/?/addConsultant",
-					dataType : 'json',
-					data: dataString,
-					success: function() {
-						$('#new_user_form').html("<div id='message'></div>");
-						$('#message').html("<h2>Konsult inlagd i systemet :D</h2>")
-						.append("<p>Vi hör av oss.</p>")
-						.hide()
-						.fadeIn(1500, function() {
-							$('#message').append("<img id='checkmark' src='/images/check.png' />");
-						});
-					}
+	$(".submit").click(function() {
+		// validate and process form
+		// first hide any error messages
+		$('.error').hide();
+		var firstname = $("input#firstname").val();
+		if (firstname == "") {
+			$("label#firstname_error").show();
+			$("input#firstname").focus();
+			return false;
+		}
+		var lastname = $("input#lastname").val();
+			if (lastname == "") {
+			$("label#lastname_error").show();
+			$("input#lastname").focus();
+			return false;
+		}
+
+		var dataString = 'firstname='+ firstname + '&lastname=' + lastname;
+		// alert (dataString); return false;
+
+		$.ajax({
+			type: "POST",
+			url: "api/?/addConsultant",
+			dataType : 'json',
+			data: dataString,
+			success: function() {
+				$('#new_user_form').html("<div id='message'></div>");
+				$('#message').html("<h2>Konsult inlagd i systemet :D</h2>")
+				.append("<p>Vi hör av oss.</p>")
+				.hide()
+				.fadeIn(1500, function() {
+					$('#message').append("<img id='checkmark' src='/images/check.png' />");
 				});
-				return false;
-			});
-		
-		};
-
+			}
+		});
+		return false;
 	});
-	
-});
+
+};
+
