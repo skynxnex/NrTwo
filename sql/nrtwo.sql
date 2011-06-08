2c50329b560fbf29c18427c83a185f3a048a42a7
Pontus Alm <pontus@almar.se>
2011-06-08 15:00:02 +0200

    updatedb

--- sql/nrtwo.sql
+++ sql/nrtwo.sql
@@ -6,7 +6,8 @@ SET time_zone = 'SYSTEM';
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
 DROP DATABASE IF EXISTS `nrtwo`;
-CREATE DATABASE `nrtwo` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_bin */;
+CREATE DATABASE `nrtwo` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_bin
+*/;
 USE `nrtwo`;
 
 DROP TABLE IF EXISTS `expertise`;
@@ -17,22 +18,31 @@ CREATE TABLE `expertise` (
   `expertise_type_id` int(10) unsigned NOT NULL,
   PRIMARY KEY (`id`),
   KEY `expertise_type_id` (`expertise_type_id`),
-  CONSTRAINT `expertise_ibfk_1` FOREIGN KEY (`expertise_type_id`) REFERENCES `expertise_type` (`id`) ON DELETE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  CONSTRAINT `expertise_ibfk_1` FOREIGN KEY (`expertise_type_id`) REFERENCES
+`expertise_type` (`id`) ON DELETE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
-INSERT INTO `expertise` (`id`, `name`, `description`, `expertise_type_id`) VALUES
-(1,	'Tyska',	'Personer som kan tyska hjälpligt',	1);
+INSERT INTO `expertise` (`id`, `name`, `description`, `expertise_type_id`)
+VALUES
+(1, 'Tyska', 'Personer som kan tyska hjälpligt', 1),
+(2, 'php', 'kan programmera php', 2),
+(3, 'java', 'kan programmera java', 2),
+(4, 'javascript', 'kan programmera javascript', 2),
+(5, 'python', 'kan programmera python', 2),
+(6, 'c++', 'kan programmera c++', 2),
+(7, 'lisp', 'kan programmera lisp', 2),
+(8, 'clojure', 'kan programmera clojure', 2);
 
 DROP TABLE IF EXISTS `expertise_type`;
 CREATE TABLE `expertise_type` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(100) COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 INSERT INTO `expertise_type` (`id`, `name`) VALUES
-(1,	'Talat språk');
-
+(1, 'Talat språk'),
+(2, 'programmeringsspråk');
 
 DROP TABLE IF EXISTS `person__expertise`;
 CREATE TABLE `person__expertise` (
@@ -40,10 +50,26 @@ CREATE TABLE `person__expertise` (
   `expertise_id` int(10) unsigned NOT NULL,
   KEY `persons_id` (`persons_id`),
   KEY `expertise_id` (`expertise_id`),
-  CONSTRAINT `person__expertise_ibfk_1` FOREIGN KEY (`persons_id`) REFERENCES `persons` (`id`) ON DELETE CASCADE,
-  CONSTRAINT `person__expertise_ibfk_2` FOREIGN KEY (`expertise_id`) REFERENCES `expertise` (`id`) ON DELETE NO ACTION
+  CONSTRAINT `person__expertise_ibfk_1` FOREIGN KEY (`persons_id`)
+REFERENCES `persons` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `person__expertise_ibfk_2` FOREIGN KEY (`expertise_id`)
+REFERENCES `expertise` (`id`) ON DELETE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
+INSERT INTO `person__expertise` (`persons_id`, `expertise_id`) VALUES
+(7, 2),
+(11, 3),
+(11, 2),
+(11, 4),
+(17, 2),
+(17, 3),
+(17, 4),
+(17, 5),
+(17, 6),
+(17, 7),
+(18, 8),
+(18, 7),
+(18, 2);
 
 DROP TABLE IF EXISTS `persons`;
 CREATE TABLE `persons` (
@@ -55,21 +81,22 @@ CREATE TABLE `persons` (
 ) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 INSERT INTO `persons` (`id`, `firstname`, `lastname`, `language_id`) VALUES
-(1,	'pelle',	'olsson',	2),
-(2,	'pelle',	'olsson',	2),
-(3,	'pelle',	'olsson',	2),
-(4,	'pelle',	'olsson',	2),
-(5,	'pelle',	'olsson',	2),
-(6,	'pelle',	'olsson',	0),
-(7,	'Pontus',	'Alm',	0),
-(8,	'pelle',	'olsson',	0),
-(9,	'pelle',	'olsson',	0),
-(10,	'pelle',	'olsson',	0),
-(11,	'olle',	'olsson',	0),
-(12,	'olle',	'olsson',	0),
-(13,	'olle',	'olsson',	0),
-(14,	'olle',	'olsson',	0),
-(17,	'Kristian',	'Grossman-Madsen',	0),
-(18,	'Hopp',	'hej',	0);
+(1, 'pelle', 'olsson', 2),
+(2, 'pelle', 'olsson', 2),
+(3, 'pelle', 'olsson', 2),
+(4, 'pelle', 'olsson', 2),
+(5, 'pelle', 'olsson', 2),
+(6, 'pelle', 'olsson', 0),
+(7, 'Pontus', 'Alm', 0),
+(8, 'pelle', 'olsson', 0),
+(9, 'pelle', 'olsson', 0),
+(10, 'pelle', 'olsson', 0),
+(11, 'olle', 'olsson', 0),
+(12, 'olle', 'olsson', 0),
+(13, 'olle', 'olsson', 0),
+(14, 'olle', 'olsson', 0),
+(17, 'Kristian', 'Grossman-Madsen', 0),
+(18, 'Hopp', 'hej', 0);
 
--- 2011-05-27 00:19:39
+-- 2011-06-08 14:44:46
+
