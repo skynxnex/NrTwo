7ad296417691d7d373dff5423035352152f37764
Hilla <hilla@hillas-dator.(none)>
2011-06-09 11:53:15 +0200

    correct database

--- sql/nrtwo.sql
+++ sql/nrtwo.sql
@@ -1,146 +1,106 @@
-﻿-- phpMyAdmin SQL Dump
--- version 3.2.0.1
--- http://www.phpmyadmin.net
---
--- Värd: localhost
--- Skapad: 09 juni 2011 kl 08:35
--- Serverversion: 5.1.36
--- PHP-version: 5.3.0
+-- Adminer 3.2.2 MySQL dump
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET NAMES utf8;
+SET foreign_key_checks = 0;
+SET time_zone = 'SYSTEM';
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
---
--- Databas: `nrtwo`
---
-DROP DATABASE `nrtwo`;
-CREATE DATABASE `nrtwo` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
+DROP DATABASE IF EXISTS `nrtwo`;
+CREATE DATABASE `nrtwo` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_bin */;
 USE `nrtwo`;
 
--- --------------------------------------------------------
-
---
--- Struktur för tabell `expertise`
---
-
 DROP TABLE IF EXISTS `expertise`;
-CREATE TABLE IF NOT EXISTS `expertise` (
+CREATE TABLE `expertise` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(50) COLLATE utf8_bin NOT NULL,
   `description` tinytext COLLATE utf8_bin,
   `expertise_type_id` int(10) unsigned NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `expertise_type_id` (`expertise_type_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=13 ;
-
---
--- Data i tabell `expertise`
---
+  KEY `expertise_type_id` (`expertise_type_id`),
+  CONSTRAINT `expertise_ibfk_1` FOREIGN KEY (`expertise_type_id`) REFERENCES `expertise_type` (`id`) ON DELETE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 INSERT INTO `expertise` (`id`, `name`, `description`, `expertise_type_id`) VALUES
-(10, 'php', 'ett kul språk', 3),
-(11, 'python', 'ett ormigt språk', 3),
-(12, 'java', 'ett annat språk', 3);
+(1,	'Tyska',	'Personer som kan tyska hjälpligt',	1),
+(2,	'php',	'kan programmera php',	2),
+(3,	'java',	'kan programmera java',	2),
+(4,	'javascript',	'kan programmera javascript',	2),
+(5,	'python',	'kan programmera python',	2),
+(6,	'c++',	'kan programmera c++',	2),
+(7,	'lisp',	'kan programmera lisp',	2),
+(8,	'clojure',	'kan programmera clojure',	2);
 
--- --------------------------------------------------------
+DROP TABLE IF EXISTS `expertise_like_expertise`;
+CREATE TABLE `expertise_like_expertise` (
+  `expertise_id` int(10) unsigned NOT NULL,
+  `like_expertise_id` int(10) unsigned NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
---
--- Struktur för tabell `expertise_islike_expertise`
---
-
-DROP TABLE IF EXISTS `expertise_islike_expertise`;
-CREATE TABLE IF NOT EXISTS `expertise_islike_expertise` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `expertise_id` int(11) NOT NULL,
-  `expertise_id2` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
-
---
--- Data i tabell `expertise_islike_expertise`
---
-
-
--- --------------------------------------------------------
-
---
--- Struktur för tabell `expertise_type`
---
+INSERT INTO `expertise_like_expertise` (`expertise_id`, `like_expertise_id`) VALUES
+(3,	4),
+(3,	5),
+(7,	8);
 
 DROP TABLE IF EXISTS `expertise_type`;
-CREATE TABLE IF NOT EXISTS `expertise_type` (
+CREATE TABLE `expertise_type` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(100) COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=4 ;
-
---
--- Data i tabell `expertise_type`
---
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 INSERT INTO `expertise_type` (`id`, `name`) VALUES
-(3, 'programmerinsspråk');
-
--- --------------------------------------------------------
-
---
--- Struktur för tabell `person__expertise`
---
+(1,	'Talat språk'),
+(2,	'programmeringsspråk');
 
 DROP TABLE IF EXISTS `person__expertise`;
-CREATE TABLE IF NOT EXISTS `person__expertise` (
+CREATE TABLE `person__expertise` (
   `persons_id` tinyint(3) unsigned NOT NULL,
   `expertise_id` int(10) unsigned NOT NULL,
-  UNIQUE KEY `persons_id_2` (`persons_id`,`expertise_id`),
   KEY `persons_id` (`persons_id`),
-  KEY `expertise_id` (`expertise_id`)
+  KEY `expertise_id` (`expertise_id`),
+  CONSTRAINT `person__expertise_ibfk_1` FOREIGN KEY (`persons_id`) REFERENCES `persons` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `person__expertise_ibfk_2` FOREIGN KEY (`expertise_id`) REFERENCES `expertise` (`id`) ON DELETE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
---
--- Data i tabell `person__expertise`
---
-
 INSERT INTO `person__expertise` (`persons_id`, `expertise_id`) VALUES
-(19, 10),
-(19, 11),
-(20, 10),
-(20, 11),
-(20, 12);
-
--- --------------------------------------------------------
-
---
--- Struktur för tabell `persons`
---
+(7,	2),
+(11,	3),
+(11,	2),
+(11,	4),
+(17,	2),
+(17,	3),
+(17,	4),
+(17,	5),
+(17,	6),
+(17,	7),
+(18,	8),
+(18,	7),
+(18,	2);
 
 DROP TABLE IF EXISTS `persons`;
-CREATE TABLE IF NOT EXISTS `persons` (
+CREATE TABLE `persons` (
   `id` tinyint(3) unsigned NOT NULL AUTO_INCREMENT,
   `firstname` varchar(50) COLLATE utf8_bin NOT NULL,
   `lastname` varchar(50) COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=21 ;
-
---
--- Data i tabell `persons`
---
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 INSERT INTO `persons` (`id`, `firstname`, `lastname`) VALUES
-(19, 'martin', 'barri'),
-(20, 'anneli', 'halldn');
+(1,	'pelle',	'olsson'),
+(2,	'pelle',	'olsson'),
+(3,	'pelle',	'olsson'),
+(4,	'pelle',	'olsson'),
+(5,	'pelle',	'olsson'),
+(6,	'pelle',	'olsson'),
+(7,	'Pontus',	'Alm'),
+(8,	'pelle',	'olsson'),
+(9,	'pelle',	'olsson'),
+(10,	'pelle',	'olsson'),
+(11,	'olle',	'olsson'),
+(12,	'olle',	'olsson'),
+(13,	'olle',	'olsson'),
+(14,	'olle',	'olsson'),
+(17,	'Kristian',	'Grossman-Madsen'),
+(18,	'Hopp',	'hej');
 
---
--- Restriktioner för dumpade tabeller
---
-
---
--- Restriktioner för tabell `expertise`
---
-ALTER TABLE `expertise`
-  ADD CONSTRAINT `expertise_ibfk_1` FOREIGN KEY (`expertise_type_id`) REFERENCES `expertise_type` (`id`) ON DELETE NO ACTION;
-
---
--- Restriktioner för tabell `person__expertise`
---
-ALTER TABLE `person__expertise`
-  ADD CONSTRAINT `person__expertise_ibfk_1` FOREIGN KEY (`persons_id`) REFERENCES `persons` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `person__expertise_ibfk_2` FOREIGN KEY (`expertise_id`) REFERENCES `expertise` (`id`) ON DELETE NO ACTION;
+-- 2011-06-09 11:51:05
